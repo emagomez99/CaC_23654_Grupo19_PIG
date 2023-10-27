d17049d6422985f0b829b6038ddf14d5336ff09c
emanuelgomez-s1 <manolo_90gomez@hotmail.com>
2023-10-27 01:54:01 -0300

    1era entrega

--- turnos/views.py
+++ turnos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def index (request):
+    return render(request, "turnos/index.html", {"hoy":datetime.now})    
+
+def servicios (request):
+
+    data = [
+        {"id": 1, "nombre": "Futbol 5", "duracion": "60 minutos", "activo": True},
+        {"id": 2, "nombre": "Futbol 11", "duracion": "105 minutos", "activo": True},
+        {"id": 3, "nombre": "Futbol infantil", "duracion": "45 minutos", "activo": True},
+    ]
+    return HttpResponse(data)
