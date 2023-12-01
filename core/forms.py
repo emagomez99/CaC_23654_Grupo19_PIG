01f09dfaf84b25bbb2242764d2d39190dbf1028d
emanuelgomez-s1 <manolo_90gomez@hotmail.com>
2023-12-01 14:02:51 -0300

    format contact

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Personaje, Compañia, Figura  #ChatGPT - revisar pa entender
+from .models import Personaje, Compañia, Figura
 
 class GreenBackgroundTextInput(forms.TextInput): #extender un widget para crear uno propio
     class Media:
@@ -12,15 +12,15 @@ class BlueBackgroundTextInput(forms.TextInput):
 
 #FORM CON VALIDACIONES EN EL FRONT Y EN EL BACK -----------------------------------------------------------------------------------------------------------------
 class ContactoForm(forms.Form): #1:09 21 models 1
-    nombre= forms.CharField(label='Nombre de contacto',widget=GreenBackgroundTextInput,required= True) #aca se ponen los placeholders
-    apellido= forms.CharField(label='Apellido de contacto', widget=forms.TextInput(attrs= {'class': 'fondo_verde'}),required= True)
-    edad= forms.IntegerField(label= 'Edad', widget=GreenBackgroundTextInput)
-    mail= forms.EmailField(label='Mail', widget=GreenBackgroundTextInput, required= True)
-  
-    nombre_pj= forms.CharField(label='Nombre de personaje',widget=BlueBackgroundTextInput,required= True)
-    fabricante= forms.CharField(label='Fabricante', widget=BlueBackgroundTextInput)
-    coleccion= forms.CharField(label='Serie/Coleccion', widget=BlueBackgroundTextInput)
-    mensaje= forms.CharField(label='Descripción', widget=forms.Textarea(attrs= {'class': 'fondo_azul'}))
+    nombre = forms.CharField(label='Nombre de contacto',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True) #aca se ponen los placeholders
+    apellido = forms.CharField(label='Apellido de contacto', widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
+    edad = forms.IntegerField(label= 'Edad', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mail = forms.EmailField(label='Mail', widget=forms.EmailInput(attrs={'class': 'form-control'}), required= True)
+
+    nombre_pj= forms.CharField(label='Nombre de personaje',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
+    fabricante= forms.CharField(label='Fabricante', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    coleccion= forms.CharField(label='Serie/Coleccion', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mensaje= forms.CharField(label='Descripción', widget=forms.Textarea(attrs= {'class': 'form-control'}))
     #imagen= forms.ImageField(label='Imagen', widget=BlueBackgroundTextInput)
 
     def clean_edad(self): #validacion a nivel de campo
@@ -40,13 +40,13 @@ class ContactoForm(forms.Form): #1:09 21 models 1
 
 # FORMULARIO ASOCIADO A UN MODELO
 class AltaFiguraForm(forms.ModelForm): #alta de figura
-    denominacion= forms.CharField(label='Denominacion',widget=GreenBackgroundTextInput,required= True)
+    denominacion= forms.CharField(label='Denominacion',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
     personaje = forms.ModelChoiceField(queryset=Personaje.objects.all(), empty_label="Seleccione un Personaje") #ChatGPT - revisar pa entender
-    coleccion= forms.CharField(label='Coleccion',widget=GreenBackgroundTextInput,required= True)
+    coleccion= forms.CharField(label='Coleccion',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
     compañias = forms.ModelMultipleChoiceField(queryset=Compañia.objects.all(), widget=forms.CheckboxSelectMultiple) #ChatGPT - revisar pa entender
-    precio= forms.FloatField(label='Precio',widget=GreenBackgroundTextInput,required= True)
-    #fecha_salida= forms.DateTimeField(label='Fecha de salida',widget=GreenBackgroundTextInput,required= True)
-    #imagen= forms.FileField(label='Imagen ilustrativa',widget=GreenBackgroundTextInput,required= True)
+    precio= forms.FloatField(label='Precio',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
+    #fecha_salida= forms.DateTimeField(label='Fecha de salida',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
+    #imagen= forms.FileField(label='Imagen ilustrativa',widget=forms.TextInput(attrs={'class': 'form-control'}),required= True)
     class Meta: #ChatGPT - revisar pa entender
         model = Figura
         fields = '__all__'
